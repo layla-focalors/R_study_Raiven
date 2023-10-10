84d8de7dd57f03836794a3b90ff6ba42654a1f0d
arisia <starlights2283@gmail.com>
2023-10-10 10:19:42 +0900

    add elseif code

---
+++ ifcode.R
@@ -0,0 +1,25 @@
+# if
+job.type = 'A'
+if(job.type == 'B'){
+  bonus <- 200
+}else{
+  bonus <- 100
+}
+job.type
+bonus
+
+# 시험문제 : 학점 처리
+score <- 85
+if(score > 90){
+  grade <- 'A'
+}else if(score > 80){
+  grade <- 'B'
+}else if(score > 70){
+  grade <- 'C'
+}else if(score > 60){
+  grade <- 'D'
+}else {
+  grade <- 'F'
+}
+
+grade
